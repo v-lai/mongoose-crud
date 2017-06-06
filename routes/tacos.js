81dcc4ab0cab817d2b6b3f3fa22f1a957d82cb18
V. Lai <lai.vivian@gmail.com>
2017-06-06 15:00:01 -0700

    add routes

--- routes/tacos.js
+++ routes/tacos.js
@@ -18,6 +18,31 @@ router.get('/new', function(req,res,next){
   })
 })
 
+router.get('/:id', function(req, res, next){
+  db.Taco.findById(req.params.id).populate('eaters').then(function(taco){
+    res.render('tacos/show', {taco});
+  // );
+  // db.Eater.findById(req.params.eater_id).populate('tacos').then(function(eater){
+  //   db.Taco.findById(req.params.id).then(function(taco){
+
+  //   }) // eater.tacos.findById(req.params.id).then(function(taco){
+  //     eval(require("locus"));
+  //     res.render('tacos/show', {eater});
+  //   // })
+    }, function(err){
+    next(err);
+  });
+});
+
+router.get('/:id/edit', function(req, res, next){
+  db.Taco.findById(req.params.id).populate('eaters').then(function(taco){
+    eval(require("locus"));
+    res.render('tacos/edit', {taco});
+  }, function(err){
+    next(err);
+  });
+});
+
 router.post('/', function(req,res,next){
   var newTaco = Object.assign({}, req.body.taco, {eater: req.params.eater_id})
   db.Taco.create(newTaco).then(function(taco){
@@ -31,6 +56,13 @@ router.post('/', function(req,res,next){
     next(err)
   })
 })
+
+router.patch('/:id', function(req, res, next){
+  db.Taco.findByIdAndUpdate(req.params.id, req.body, {new:true}).then(function(){
+    eval(require("locus"));
+    res.redirect(`/eaters/${req.params.eater_id}/tacos`)
+  });
+});
 
 
 module.exports = router;
